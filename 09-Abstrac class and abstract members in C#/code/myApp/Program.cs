4f3425e9e7f19cbbeed2fcecd7f6a3b0b3dcb62f
Nguyen Duc Hoang <sunlight4d@gmail.com>
2018-03-16 14:33:48 +0700

    update

---
+++ 09-Abstrac class and abstract members in C#/code/myApp/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+ Mr Nguyen Duc Hoang
+ https://www.youtube.com/c/nguyenduchoang
+ Programming videos in .NET core, and many more....
+*/
+using System;
+
+namespace myApp
+{    
+    public abstract class Animal 
+    {
+        protected abstract string Name { get; set; }
+        public abstract void MakeNoise();
+    }
+    public class Cat: Animal 
+    {
+        private string name;//This is a field, "backing store"
+        public override void MakeNoise()
+        {
+            Console.WriteLine($"{this.Name} says: Meao meao");
+        }
+        public Cat(string aName)
+        {
+            this.name = aName;
+        }
+        //public abstract void Eat();
+        protected sealed override string Name
+        {
+            get
+            {
+                return this.name.ToUpper();
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var animal = new Animal();//Cannot initialize an Abstract class
+            //Animal animal = new Animal();//Cannot initialize an Abstract class
+            Cat cat = new Cat("Mimi");
+            cat.MakeNoise();
+        }
+    }
+}
