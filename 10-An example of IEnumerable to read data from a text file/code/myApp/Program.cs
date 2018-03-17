bc8e281110367444f94700095bcc8cef89c59314
Nguyen Duc Hoang <sunlight4d@gmail.com>
2018-03-17 14:07:02 +0700

    update

---
+++ 10-An example of IEnumerable to read data from a text file/code/myApp/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+ Mr Nguyen Duc Hoang
+ https://www.youtube.com/c/nguyenduchoang
+ Programming videos in .NET core, and many more....
+*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace myApp
+{    
+    public class Point
+    {
+        public Double X { get; set; }
+        public Double Y { get; set; }
+        public Point(Double x, Double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"x = {this.X}, y = {this.Y}");
+        }
+    }
+    class Program
+    {
+        static IEnumerable<string> ReadFrom(string fileName)
+        {
+            var reader = File.OpenText(fileName);
+            //Console.WriteLine($"Type of reader = {reader.GetType()}");
+            string eachLine;
+            while ((eachLine = reader.ReadLine()) != null)
+            {
+                yield return eachLine;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            var lines = ReadFrom("textFile.txt");
+            var points = new List<Point>();
+            foreach (var line in lines)
+            {
+                //Console.WriteLine(line);
+                var numbers = line.Split(new[] { ", " }, StringSplitOptions.None);
+                var x = Convert.ToDouble(numbers[0]);
+                var y = Convert.ToDouble(numbers[1]);
+                var newPoint = new Point(x, y);
+                points.Add(newPoint);
+            }
+            Console.WriteLine("List of points: ");
+            foreach (var point in points)
+            {
+                point.ShowInfo();
+            }
+        }
+    }
+}
---
+++ 11-LINQ#1.Introduction to LINQ Queries in C#/code/myApp/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+ Mr Nguyen Duc Hoang
+ https://www.youtube.com/c/nguyenduchoang
+ Programming videos in .NET core, and many more....
+*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace myApp
+{        
+    class LinQExample
+    {
+        public static void queryIntNumbers()
+        {
+            //  1. Data source.
+            int[] numbers = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            // 2. Query creation 
+            var queryResult =
+                from number in numbers
+                where (number % 2) == 0
+                select number;
+            // 3. Query execution.
+            foreach(int number in queryResult) //IEnumerable<int>
+            {
+                Console.Write($"{number} ");
+            }
+            Console.WriteLine("Convert query's result to array");
+            var arrayOfNumbers = queryResult.ToArray();
+            foreach (int number in arrayOfNumbers) //This is an array
+            {
+                Console.Write($"{number} ");
+            }
+            Console.WriteLine("Convert query's result to list");
+            var listOfNumbers = queryResult.ToList();
+            foreach (int number in listOfNumbers) //This is a List
+            {
+                Console.Write($"{number} ");
+            }
+            Console.WriteLine("End of conversion !");
+        }
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            LinQExample.queryIntNumbers();
+        }
+    }
+}
